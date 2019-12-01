09906027aca0e8efb3022b077fabb07452028dd9
Bradon Thymes <bmt.wcpa@gmail.com>
2019-12-01 18:16:33 -0500

    Recursion added

---
+++ Recursion/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int countdown(int num);
+int sumRange(int num);
+
+int main()
+{
+    int a = 10;
+    countdown(a);
+
+    int b = 3;
+    cout << sumRange(b) << endl;
+
+    return 0;
+}
+
+int countdown(int num)
+{
+    if (num <= 0)
+    {
+        cout << "Finished!\n";
+        return 0;
+    }
+    cout << num << endl;
+    num--;
+    return countdown(num);
+}
+
+int sumRange(int num)
+{
+    if (num == 1)
+    {
+        cout << "Finished!\n";
+        return 1;
+    }
+    cout << num << endl;
+    return num + sumRange(num - 1);
+}
