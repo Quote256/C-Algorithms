9cf1c1f4adbeabfd6b16414cb68a15e6ce87f508
Bradon Thymes <bmt.wcpa@gmail.com>
2020-07-27 13:24:40 -0400

    Started working on  arrays

---
+++ Data_Structures/Arrays/main.cpp
@@ -0,0 +1,11 @@
+//This is example code for an array data structure
+#include <iostream>
+
+int main()
+{
+    int array[] = {1, 2, 3, 4, 5};
+    //Print out the contents of one element of an array
+    std::cout << array[0] << std::endl;
+
+    return 0;
+}
